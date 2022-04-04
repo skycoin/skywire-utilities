ae534870f5952c99a305c5a98c664e1308264a3d
ersonp <ersonpereiracr7@gmail.com>
2022-04-04 16:18:58 +0530

    Add struct HealthCheckResponse
    This commit adds the struct HealthCheckResponse which is used by all the services in the /health endpoint.

--- pkg/httputil/health.go
+++ pkg/httputil/health.go
@@ -1,1 +1,13 @@
 package httputil
+
+import (
+	"time"
+
+	"github.com/skycoin/skywire-utilities/pkg/buildinfo"
+)
+
+// HealthCheckResponse is struct of /health endpoint
+type HealthCheckResponse struct {
+	BuildInfo *buildinfo.Info `json:"build_info,omitempty"`
+	StartedAt time.Time       `json:"started_at"`
+}
