1885866db6201a57a4dd19bdf8f512d599fcd0fd
ersonp <ersonpereiracr7@gmail.com>
2022-04-26 17:31:01 +0530

    Remove prefix from struct redisStore
    This commit removes the field prefix from the struct redisStore as it is not required.
    
    The method key is also removed.

--- pkg/httpauth/redis-store.go
+++ pkg/httpauth/redis-store.go
@@ -13,7 +13,6 @@ import (
 
 type redisStore struct {
 	client *redis.Client
-	prefix string
 }
 
 func newRedisStore(addr, password string) (*redisStore, error) {
@@ -41,12 +40,8 @@ func newRedisStore(addr, password string) (*redisStore, error) {
 	return store, nil
 }
 
-func (s *redisStore) key(v string) string {
-	return s.prefix + v
-}
-
 func (s *redisStore) Nonce(_ context.Context, remotePK cipher.PubKey) (Nonce, error) {
-	nonce, err := s.client.Get(s.key(fmt.Sprintf("nonces:%s", remotePK))).Result()
+	nonce, err := s.client.Get(fmt.Sprintf("nonces:%s", remotePK)).Result()
 	if err != nil {
 		return 0, nil
 	}
@@ -59,12 +54,12 @@ func (s *redisStore) Nonce(_ context.Context, remotePK cipher.PubKey) (Nonce, er
 }
 
 func (s *redisStore) IncrementNonce(_ context.Context, remotePK cipher.PubKey) (Nonce, error) {
-	nonce, err := s.client.Incr(s.key(fmt.Sprintf("nonces:%s", remotePK))).Result()
+	nonce, err := s.client.Incr(fmt.Sprintf("nonces:%s", remotePK)).Result()
 	if err != nil {
 		return 0, fmt.Errorf("redis: %w", err)
 	}
 
-	_, err = s.client.SAdd(s.key("nonces"), remotePK).Result()
+	_, err = s.client.SAdd("nonces", remotePK).Result()
 	if err != nil {
 		return 0, fmt.Errorf("redis: %w", err)
 	}
@@ -73,7 +68,7 @@ func (s *redisStore) IncrementNonce(_ context.Context, remotePK cipher.PubKey) (
 }
 
 func (s *redisStore) Count(ctx context.Context) (n int, err error) {
-	size, err := s.client.SCard(s.key("nonces")).Result()
+	size, err := s.client.SCard("nonces").Result()
 	if err != nil {
 		return 0, fmt.Errorf("redis: %w", err)
 	}
